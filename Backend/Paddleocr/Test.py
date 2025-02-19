66130c42652c9e5112e23ec3dfbf68b2f955adaf
Shanmuka Srinivas <shanmuka srinivasa>
2025-02-19 22:18:28 +0530

    changed Time

--- Backend/Paddleocr/Test.py
+++ Backend/Paddleocr/Test.py
@@ -17,10 +17,13 @@ import time
 
 def inference(image_path):
     ocr = PaddleOCR(rec_model_dir="infrence2",use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
+    start_time = time.time()
     result = ocr.ocr(image_path, cls=True)
     result = result[0]
     boxes = [line[0] for line in result]
     txts = [line[1][0] for line in result]
     scores = [line[1][1] for line in result]
+    end_time = time.time()
+    execution_time = end_time - start_time
     print(txts)
-    return txts, scores
+    return txts, scores,execution_time
